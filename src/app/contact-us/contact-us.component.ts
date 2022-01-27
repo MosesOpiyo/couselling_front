95ccae575824d852e6992470d90922b25ba3172f
MosesOpiyo <86948682+MosesOpiyo@users.noreply.github.com>
2022-01-27 14:25:15 +0300

    Revert "Homepage"

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -6,8 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-us.component.css']
 })
 export class ContactUsComponent implements OnInit {
-  model: any = {};
-
 
   constructor() { }
 
